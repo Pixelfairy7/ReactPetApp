71dee0df0fba8b4fdcbfaeeb8e7940c62e68cccd
Erika Myles <erikamyles314@gmail.com>
2020-01-29 18:32:29 -0500

    initial addns

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.scss';
+
+function Header() {
+  return (
+    <header className="header">
+        <h1> Hello</h1>
+        <h2> Hi </h2>
+    </header>
+    
+  );
+}
+
+export default Header;
